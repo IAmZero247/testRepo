2db39680f688f43dfab04221ab301c4eb12fdff2
IAmZero247 <76902571+IAmZero247@users.noreply.github.com>
2021-05-09 14:35:24 +0530

    Update Test1.java

--- Test1.java
+++ Test1.java
@@ -64,6 +64,13 @@ public class Test1 {
 //    empid,deptid, amt~
 //     1,IT,400~
 //     4,OPS,320~
+    
+    
+    /* logs ----
+     * "C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2019.3.3\jbr\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2019.3.3\lib\idea_rt.jar=57607:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2019.3.3\bin" -Dfile.encoding=UTF-8 -classpath "C:\Users\Sandeep.KS\OneDrive - EY\Desktop\lib\New folder\java8\target\classes;C:\Users\Sandeep.KS\.m2\repository\org\jooq\jool\0.9.14\jool-0.9.14.jar;C:\Users\Sandeep.KS\.m2\repository\com\diogonunes\JColor\5.0.1\JColor-5.0.1.jar;C:\Users\Sandeep.KS\.m2\repository\net\java\dev\jna\jna\5.5.0\jna-5.5.0.jar;C:\Users\Sandeep.KS\.m2\repository\net\java\dev\jna\jna-platform\5.5.0\jna-platform-5.5.0.jar;C:\Users\Sandeep.KS\.m2\repository\org\apache\commons\commons-lang3\3.11\commons-lang3-3.11.jar;C:\Users\Sandeep.KS\.m2\repository\com\github\javafaker\javafaker\0.12\javafaker-0.12.jar;C:\Users\Sandeep.KS\.m2\repository\com\github\mifmif\generex\1.0.2\generex-1.0.2.jar;C:\Users\Sandeep.KS\.m2\repository\dk\brics\automaton\automaton\1.11-8\automaton-1.11-8.jar" test.Temp1
+     * [Emp{eid=4, did='OPS', amt=320}, Emp{eid=1, did='IT', amt=400}]
+     * Process finished with exit code 0
+     */
 
 
     public static void main(String[] args) {
